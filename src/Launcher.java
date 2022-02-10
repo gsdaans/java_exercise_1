20f8366f4a3317a957e2e86f19f3cf2c967a6dc4
gsdaans <gs.daans@gmail.com>
2022-02-10 12:59:06 +0100

    Partie 4, fbo

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,6 +2,15 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibo(int n) {
+        if (Objects.equals(n, 0))
+            return 0;
+        if (Objects.equals(n, 1))
+            return 1;
+        if (n > 1)
+            return (fibo(n-1) + fibo(n - 2));
+        return n;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome !\nType a command :");
 
@@ -11,6 +20,12 @@ public class Launcher {
             String instruction = scanner.nextLine();
             if (Objects.equals(instruction, "quit"))
                 break;
+            else if (Objects.equals(instruction, "fibo")) {
+                System.out.println("Enter the index of Fibonacci value you want :");
+                int fiboval = scanner.nextInt();
+                instruction = scanner.nextLine();
+                System.out.println(fibo(fiboval));
+            }
             else System.out.println("Unknown command");
         }
     }
